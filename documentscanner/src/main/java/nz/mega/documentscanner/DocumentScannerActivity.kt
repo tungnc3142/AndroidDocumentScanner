1f3785edc3e031047d16700c1e67179d061ab1d6
Gonzalo Toledano <gonzalotldn@gmail.com>
2020-09-17 11:03:01 +0200

    Moved initialisation logic to activity level

--- documentscanner/src/main/java/nz/mega/documentscanner/DocumentScannerActivity.kt
+++ documentscanner/src/main/java/nz/mega/documentscanner/DocumentScannerActivity.kt
@@ -2,11 +2,14 @@ package nz.mega.documentscanner
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import nz.mega.documentscanner.databinding.ActivityDocumentScannerBinding
+import nz.mega.documentscanner.utils.FileUtils
+import org.opencv.android.OpenCVLoader
 
 class DocumentScannerActivity : AppCompatActivity() {
 
@@ -25,7 +28,20 @@ class DocumentScannerActivity : AppCompatActivity() {
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initOpenCV()
+        clearExistingFiles()
+
         binding = ActivityDocumentScannerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun initOpenCV() {
+        val result = OpenCVLoader.initDebug()
+        Log.d(TAG, "OpenCV initialized: $result")
+    }
+
+    private fun clearExistingFiles() {
+        val result = FileUtils.clearExistingFiles(this)
+        Log.d(TAG, "Cleared existing files: $result")
     }
 }
